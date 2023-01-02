d8ea9ca52647fb0483af478266a0c2de1f3da2b9
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-02 12:44:21 +0300

    Import the HTTP and the CORS library to allow data transfer between the client and the server domains

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+// import HTTP and CORS library
+const http = require("http").Server(app);
+const cors = require("cors");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -10,3 +14,5 @@ app.get("/api", (req, res) => {
     message: "Hello from server!",
   });
 });
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
