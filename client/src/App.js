917df913ea287b8fdd5167fa6b86e07208b87f05
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-02 13:05:35 +0300

    Add routes in the app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import UploadPhoto from "./components/UploadPhoto";
+import MyPhotos from "./components/MyPhotos";
+import SharePhoto from "./components/SharePhoto";
+import Photos from "./components/Photos";
+
 import { io } from "socket.io-client";
 import "./App.css";
 
-
-//👇🏻 http://localhost:4000 is where the server host URL.
+// http://localhost:4000 is where the server host URL.
 const socket = io.connect("http://localhost:4000");
 
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login socket={socket} />} />
+          <Route path="/register" element={<Register socket={socket} />} />
+          <Route path="/photos" element={<Photos socket={socket} />} />
+          <Route
+            path="/photos/upload"
+            element={<UploadPhoto socket={socket} />}
+          />
+          <Route path="/user/photos" element={<MyPhotos socket={socket} />} />
+          <Route path="/share/:user" element={<SharePhoto socket={socket} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
