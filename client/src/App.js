c85759dae3bdc11a8ef8eabe24f58bc8bbd30110
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-02 12:56:30 +0300

    create the required components

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { io } from "socket.io-client";
+import "./App.css";
+
+
+//👇🏻 http://localhost:4000 is where the server host URL.
+const socket = io.connect("http://localhost:4000");
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React App</h1>
     </div>
   );
 }
